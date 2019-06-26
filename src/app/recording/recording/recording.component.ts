5069f9337ec3363644c00120a44ebbf7b6c4a643
kawakami <kkawakami@smartshopping.co.jp>
2019-06-26 21:20:09 +0900

    レコーディング

--- src/app/recording/recording/recording.component.ts
+++ src/app/recording/recording/recording.component.ts
@@ -1,9 +1,7 @@
 import {Component, OnInit} from '@angular/core';
 import {animate, keyframes, state, style, transition, trigger} from '@angular/animations';
 import {SoundService} from '../../service/sound.service';
-import {AppDispatcher} from '../../walts/app.dispatcher';
-import {AppActions} from '../../walts/app.actions';
-import {AppStore} from '../../walts/app.store';
+import {RecordingService} from '../../service/recording.service';
 
 declare var MediaRecorder: any;
 
@@ -26,11 +24,11 @@ declare var MediaRecorder: any;
         'background-color': '#888',
       })),
       state('stop', style({
-        'background-image': 'url("/assets/images/rec.svg")',
+        'background-image': 'url("/assets/images/play.svg")',
         'background-color': '#888',
       })),
       state('rec', style({
-        'background-image': 'url("/assets/images/stop.svg")',
+        'background-image': 'url("/assets/images/rec.svg")',
         'background-color': '#f44',
       })),
       transition('rec => stop', [
@@ -47,11 +45,13 @@ export class RecordingComponent implements OnInit {
   recordingState: string;
   mediaRecorder: any;
   recordedChunks: any[] = [];
+  countDownInterval: any;
 
-  constructor(private sound: SoundService,
-              private dispatcher: AppDispatcher,
-              private actions: AppActions,
-              private store: AppStore) {
+  constructor(
+      private soundService: SoundService,
+      private recordingService: RecordingService,
+  ) {
+    this.recordingState = 'stop';
   }
 
   ngOnInit() {
@@ -59,7 +59,29 @@ export class RecordingComponent implements OnInit {
     // https://developers.google.com/web/updates/2016/01/mediarecorder
     navigator.getUserMedia({audio: true},
         stream => {
-          this.mediaRecorder                 = new MediaRecorder(stream, {mimeType: 'audio/webm'});
+          this.mediaRecorder = new MediaRecorder(stream, {mimeType: 'audio/webm'});
+
+          this.mediaRecorder.onstop = (e) => {
+            const superBuffer = new Blob(this.recordedChunks);
+
+            // Sound URL
+            const recordingSoundUrl: string = window.URL.createObjectURL(superBuffer);
+
+            // BEAT再生用
+            this.soundService.recording = recordingSoundUrl;
+
+            // reset chunks
+            this.recordedChunks = [];
+
+            const audio: HTMLAudioElement = new Audio(recordingSoundUrl);
+
+            // PLAY Sound
+            setTimeout(() => {
+              audio.play();
+            }, 1000);
+
+          };
+
           this.mediaRecorder.ondataavailable = (event: any) => {
             if (event.data.size > 0) {
               this.recordedChunks.push(event.data);
@@ -69,65 +91,58 @@ export class RecordingComponent implements OnInit {
         err => console.log('The following error occurred: ' + err.name),
     );
 
-    // Walths
-    this.store.getRecordingState().subscribe(val => {
-      this.recordingState = val;
-
-      switch (this.recordingState) {
+    this.recordingService.recording.subscribe((res: string) => {
+      switch (res) {
         case 'rec':
-          setTimeout(() => this.dispatcher.emit(this.actions.changeRecordingState()), 400);
-          this.mediaRecorder.start();
+          let count = 3;
+
+          this.countDownInterval = setInterval(() => {
+            switch (count) {
+              case 3:
+                this.recordingState = 'three';
+                break;
+
+              case 2:
+                this.recordingState = 'two';
+                break;
+
+              case 1:
+                this.recordingState = 'one';
+                break;
+
+              case 0:
+                this.recordingState = 'rec';
+
+                clearInterval(this.countDownInterval);
+
+                this.mediaRecorder.start();
+
+                setTimeout(() => {
+                      this.recordingService.switchRecording();
+                    },
+                    400);
+            }
+
+            count--;
+          }, 600);
+
           break;
 
         case 'stop':
-          if (this.mediaRecorder) {
+          clearInterval(this.countDownInterval);
 
+          if (this.mediaRecorder.state === 'recording') {
             this.mediaRecorder.stop();
-
-            const superBuffer: Blob = new Blob(this.recordedChunks);
-
-            // Sound URL
-            const recordingSoundUrl: string = window.URL.createObjectURL(superBuffer);
-
-            // Service
-            this.sound.recording = recordingSoundUrl;
-
-            // reset chunks
-            this.recordedChunks = [];
-
-            // PLAY Sound
-            setTimeout(() => {
-              const audio: HTMLAudioElement = new Audio(recordingSoundUrl);
-              audio.play();
-            }, 1000);
           }
+
+          this.recordingState = 'stop';
+
           break;
       }
     });
   }
 
   private onClick() {
-
-    let count       = 3;
-    const countDown = setInterval(() => {
-
-      switch (count) {
-        case 3:
-          this.recordingState = 'three';
-          break;
-        case 2:
-          this.recordingState = 'two';
-          break;
-        case 1:
-          this.recordingState = 'one';
-          break;
-        case 0:
-          this.dispatcher.emit(this.actions.changeRecordingState());
-          clearInterval(countDown);
-      }
-
-      count--;
-    }, 600);
+    this.recordingService.switchRecording();
   }
-
 }
--- src/app/walts/app.state.ts
+++
@@ -1,7 +0,0 @@
-import {State} from 'walts';
-
-export interface AppState extends State {
-    //
-    recording: string,
-    recording_sound: string,
-}
