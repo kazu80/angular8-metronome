2deb990d23ae6533ade0581fcc4f7f45758ef007
Kazuyoshi Kawakami <saruyama.monki@gmail.com>
2019-07-01 07:49:40 +0900

    pixi

--- src/app/metronome/tempo-display/tempo-display.component.ts
+++ src/app/metronome/tempo-display/tempo-display.component.ts
@@ -1,4 +1,6 @@
-import {Component, Input, OnInit} from '@angular/core';
+import {Component, ElementRef, Input, OnInit} from '@angular/core';
+
+declare var PIXI: any;
 
 @Component({
   selector: 'app-tempo-display',
@@ -6,13 +8,37 @@ import {Component, Input, OnInit} from '@angular/core';
   styleUrls: ['./tempo-display.component.scss']
 })
 export class TempoDisplayComponent implements OnInit {
-  @Input() tempo: number;
+  @Input() set tempo(value: number) {
+    this.tempoValue = value;
+    if (this.tempoPixiText !== undefined) {
+      this.tempoPixiText.text = value;
+    }
+  }
   @Input() runStatus: string;
 
+  tempoPixiText: any;
+  tempoValue: number;
+  private dom: HTMLElement;
 
-  constructor() { }
+
+  constructor(el: ElementRef) {
+    this.dom = el.nativeElement;
+  }
 
   ngOnInit() {
+    const app = new PIXI.Application({
+      view: this.dom.querySelector('#tempo-display')
+    });
+
+    app.renderer.backgroundColor = 0xffffff;
+
+    const style   = new PIXI.TextStyle({
+      fontFamily: 'digital',
+      fontSize: 110
+    });
+    this.tempoPixiText = new PIXI.Text(this.tempoValue, style);
+
+    app.stage.addChild(this.tempoPixiText);
   }
 
 }
