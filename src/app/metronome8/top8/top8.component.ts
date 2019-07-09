10fdea7f2bd57f3a38d9a8af0e6a80eadf30c390
kawakami <kkawakami@smartshopping.co.jp>
2019-07-10 01:22:56 +0900

    pixi

--- src/app/metronome8/top8/top8.component.ts
+++ src/app/metronome8/top8/top8.component.ts
@@ -1,4 +1,5 @@
 import {Component, ElementRef, OnInit} from '@angular/core';
+import {Subject} from 'rxjs';
 
 declare var PIXI: any;
 
@@ -9,41 +10,61 @@ declare var PIXI: any;
 })
 export class Top8Component implements OnInit {
   private dom: HTMLElement;
+
   private displayHeight: number;
   private displayWidth: number;
   private displayColor: string;
 
-  constructor(el: ElementRef) {
-    this.dom = el.nativeElement;
+  private scene: Subject<number>;
 
-    this.displayHeight = window.innerHeight - 67;
-    this.displayWidth  = window.innerWidth;
-    this.displayColor = '0xf2f2f2';
+  constructor(el: ElementRef) {
+      this.scene = new Subject();
+
+      this.dom = el.nativeElement;
+
+      this.displayHeight = window.innerHeight - 67;
+      this.displayWidth  = window.innerWidth;
+      this.displayColor = '0xf2f2f2';
   }
 
   ngOnInit() {
-    const app = new PIXI.Application({
-      width: this.displayWidth,
-      height: this.displayHeight,
-      backgroundColor: this.displayColor
+    this.scene.asObservable().subscribe((scene) => {
+        switch (scene) {
+            case 1:
+                this.scene1();
+                break;
+        }
     });
 
-    this.dom.querySelector('#display').appendChild(app.view);
+    this.scene.next(1);
+  }
 
-    app.loader.add('logo', '/assets/images/angular8/logo.png').load(
-        (loader, resorces) => {
-          const logo = new PIXI.Sprite(resorces.logo.texture);
+  scene1() {
+      const app = new PIXI.Application({
+          width: this.displayWidth,
+          height: this.displayHeight,
+          backgroundColor: this.displayColor
+      });
 
-          logo.x = app.renderer.width / 2;
-          logo.y = app.renderer.height / 2;
+      this.dom.querySelector('#display').appendChild(app.view);
 
-          // Rotate around the center
-          logo.anchor.x = 0.5;
-          logo.anchor.y = 0.5;
+      app.loader.add('logo', '/assets/images/angular8/logo.png').load(
+          (loader, resorces) => {
+              const logo = new PIXI.Sprite(resorces.logo.texture);
 
-          // Add the bunny to the scene we are building
-          app.stage.addChild(logo);
-        }
-    );
+              logo.x = app.renderer.width / 2;
+              logo.y = app.renderer.height / 2;
+
+              // Rotate around the center
+              logo.anchor.x = 0.5;
+              logo.anchor.y = 0.5;
+
+              logo.scale.x = 0.8;
+              logo.scale.y = 0.8;
+
+              // Add the bunny to the scene we are building
+              app.stage.addChild(logo);
+          }
+      );
   }
 }
